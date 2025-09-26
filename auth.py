13a6e3f0dcebcf2382b7b8deb948e0e003097f9c
cac-souza <cleberacsouza@gmail.com>
2025-09-26 16:51:30 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 def exigir_login():
-    # Checa se existe um usuário logado
+    # Checa se existe um usuário logado - 
     if st.session_state.get("usuario") is None:
         st.warning("⚠️ Você precisa estar logado para acessar esta aba.")
         st.stop()
+
 
 
